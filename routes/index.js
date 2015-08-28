9170f4ee11e85e26e0e797a47d7120a3a9fedbe6
cp <chengpengcp9@163.com>
2015-08-28 14:10:19 +0800

    first

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'index' });
+    res.render('index/index', { title: 'index' });
 });
 
 module.exports = router;
